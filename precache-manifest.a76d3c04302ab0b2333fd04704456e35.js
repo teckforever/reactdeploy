432f49d955bcbbce6c479440b580be4dc770ac7d
luis moreno <teckforever@gmail.com>
2020-09-17 17:14:33 -0500

    Updates

---
+++ precache-manifest.a76d3c04302ab0b2333fd04704456e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78f046e696d099f96e05441bb1115b3",
+    "url": "/reactdeploy/index.html"
+  },
+  {
+    "revision": "e875d3540ed612ab6ad3",
+    "url": "/reactdeploy/static/css/main.3720816f.chunk.css"
+  },
+  {
+    "revision": "a14c383937ca16e8e0f9",
+    "url": "/reactdeploy/static/js/2.5044d7fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactdeploy/static/js/2.5044d7fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e875d3540ed612ab6ad3",
+    "url": "/reactdeploy/static/js/main.ee751894.chunk.js"
+  },
+  {
+    "revision": "1e9e7f9e6cfbbea18df1",
+    "url": "/reactdeploy/static/js/runtime-main.e293b56b.js"
+  }
+]);
